728f8a779e9a39ece4a5a916576d1e7eab2e79b4
VanshikaSahu <82844285+VanshikaSahu@users.noreply.github.com>
2021-05-06 18:18:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+  const[state, setState] = useState();
+  const renderPara = () =>{
+    setState(`Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick = {renderPara} id = "click" >Render</button>
+      <p id = 'para'>{state}</p>
     </div>
   );
 }
